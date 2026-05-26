74baa71e5ac09a80116e83a97357b6b2529ac466
Praveenvishwaka <praveen.vishwakarma@vwo.com>
2026-05-26 21:25:57 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       script: [
         {
           id: 'vwoCode',
-          src: 'https://edge.wingify.net/tag/1055484.js'
+          src: 'https://dev.visualwebsiteoptimizer.com/tag/1055484.js'
         }
       ]
     }
